2bd53c25573b2ba9fa6ad6fcbe3db8ff992e7236
Baird Rouan S. Buenaventura <bairdbuenaventura@gmail.com>
2025-04-18 16:27:31 +0800

    Version 2 - Training Application 2
    Version 2 - Training Application 2
    Covers Module
    [Interact with data in Blazor web apps](https://learn.microsoft.com/en-us/training/modules/interact-with-data-blazor-web-apps/)

--- TrainingApp1/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ TrainingApp1/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1067394f21f82b958585efa88b89c2610d8a174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50c1765179f3b04cad22ab06a0a70ddfec7ce68")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
